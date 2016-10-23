20bf90c0d5dc1b389738fe094c11caf22d85cdbd
AythaE <aythae@gmail.com>
2016-10-23 12:40:34 +0200

    Añadida clase Interfaz con comentarios

--- Practica_1/manejadorEventosAsincronos/src/main/java/es/ugr/dss/Interfaz.java
+++ Practica_1/manejadorEventosAsincronos/src/main/java/es/ugr/dss/Interfaz.java
@@ -1,3 +1,12 @@
+/*
+ * Archivo: Interfaz.java 
+ * Proyecto: manejadorEventosAsincronos
+ * 
+ * Autor: Aythami Estévez Olivas
+ * Email: aythae@correo.ugr.es
+ * Fecha: 23-oct-2016
+ * Asignatura: Desarrollo de Sistemas Software basados en Componentes y Servicios
+ */
 package es.ugr.dss;
 
 import java.awt.Desktop;
@@ -5,9 +14,30 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+/**
+ * Clase Interfaz encargada de instanciar la interfaz de usuario de la aplicación
+ * al ser invocado su método ejecutar como objetivo de la cadena de filtros
+ * @author aythae
+ *
+ */
 public class Interfaz {
 
 	
+	/**
+	 * Método ejecutar invocado por {@link CadenaFiltros} al ser una instancia
+	 * de esta clase su objetivo.
+	 * 
+	 * <p>
+	 * Crea una {@link URI} con la dirección donde se encuentra accesible la 
+	 * aplicacion desplegada en un tomcat e intena abrirla en el navegador 
+	 * mediante el método {@link Desktop#browse(URI)}, en caso de no poder
+	 * crea un hilo que duerme 2 segundos y muestra en el terminal que no se ha
+	 * podido abrir el navegador pidiendo al usuario que lo haga él mismo y 
+	 * proporcionandole la URL a la que debe acceder
+	 *
+	 * @param o Parametro de tipo Object para que pueda ser cualquier tipo
+	 * 			(No se utiliza)
+	 */
 	public void ejecutar(Object o) {
 		
 			String webAppAddr= "http://localhost:8080/manejadorEventosAsincronos/home.jsf";
@@ -26,7 +56,7 @@ public class Interfaz {
 								try {
 									Thread.sleep(2000);
 								} catch (InterruptedException e) {
-									// TODO Auto-generated catch block
+									
 									e.printStackTrace();
 								}
 								System.out.println("\n\nNo se ha podido abrir el navegador por defecto, "+
@@ -42,10 +72,10 @@ public class Interfaz {
 				
 
 			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			}
 	}	
